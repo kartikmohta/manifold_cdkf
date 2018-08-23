88e5c0b0a839f7c1b0610f2c56a406b3f22feb5c
Kartik Mohta <kartikmohta@gmail.com>
2018-08-22 21:11:08 -0400

    base.hpp: Add default copy and move constructors + assignment operators

--- include/manifold_cdkf/element_types/base.hpp
+++ include/manifold_cdkf/element_types/base.hpp
@@ -9,7 +9,18 @@ class ManifoldElement
   using Scalar_t = Scalar;
   using TangentVec = Eigen::Matrix<Scalar, tangent_dim, 1>;
 
+  ManifoldElement() = default;
   virtual ~ManifoldElement() = default;
+
+  // See https://stackoverflow.com/a/4782927 or
+  // https://en.cppreference.com/w/cpp/language/rule_of_three#Rule_of_zero
+  ManifoldElement(const ManifoldElement &) = default; // Copy constructor
+  ManifoldElement(ManifoldElement &&) = default;      // Move constructor
+  ManifoldElement &operator=(const ManifoldElement &) & =
+      default; // Copy assignment operator
+  ManifoldElement &operator=(ManifoldElement &&) & =
+      default; // Move assignment operator
+
   virtual Derived operator+(const TangentVec &diff) const = 0;
   virtual TangentVec operator-(const Derived &element) const = 0;
 
