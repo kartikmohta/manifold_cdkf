df2eb62df11d6f8ba87f0abc7ab2003e4feea4c1
Kartik Mohta <kartikmohta@gmail.com>
2018-09-28 03:17:10 -0400

    Add SO2 element

---
+++ include/manifold_cdkf/element_types/so2.hpp
@@ -0,0 +1,45 @@
+#include "base.hpp"
+
+template <typename Scalar>
+class SO2Element final : public ManifoldElement<SO2Element<Scalar>, Scalar, 1>
+{
+ public:
+  using Base = ManifoldElement<SO2Element<Scalar>, Scalar, 1>;
+  using TangentVec = typename Base::TangentVec;
+
+  using ElementType = Scalar;
+
+  SO2Element(const Scalar &angle = Scalar{0}) : angle_{normalize_angle(angle)}
+  {
+  }
+
+  Scalar getValue() const { return angle_; }
+  void setValue(const Scalar &angle) { angle_ = normalize_angle(angle); }
+
+  SO2Element operator+(const TangentVec &diff) const override
+  {
+    return SO2Element(normalize_angle(angle_ + diff(0)));
+  }
+
+  TangentVec operator-(const SO2Element &other) const override
+  {
+    return TangentVec(normalize_angle(angle_ - other.angle_));
+  }
+
+  friend std::ostream &operator<<(std::ostream &stream,
+                                  const SO2Element &element)
+  {
+    stream << element.angle_;
+    return stream;
+  }
+
+ private:
+  static constexpr Scalar normalize_angle(Scalar angle)
+  {
+    constexpr Scalar pi = M_PI, two_pi = 2 * M_PI;
+    return angle - (std::ceil((angle + pi) / two_pi) - Scalar{1}) * two_pi;
+  }
+
+  Scalar angle_;
+  static constexpr Scalar pi = M_PI;
+};
---
+++ tests/test_so2.cpp
@@ -0,0 +1,52 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+#include <random>
+
+#include "manifold_cdkf/element_types/so2.hpp"
+
+using Scalar_t = double;
+
+static std::random_device rd;
+static std::mt19937 gen(rd());
+static std::uniform_real_distribution<Scalar_t> dis(-M_PI, M_PI);
+// Note that we want numbers from (-M_PI, M_PI] hence we use -dis(gen) to get the random number
+
+static auto const elem_zero = SO2Element<Scalar_t>(0);
+static auto const elem_pi_2 = SO2Element<Scalar_t>(M_PI / 2);
+static auto const elem_pi = SO2Element<Scalar_t>(M_PI);
+static auto const elem_m_pi_2 = SO2Element<Scalar_t>(-M_PI / 2);
+static auto const elem_m_pi = SO2Element<Scalar_t>(-M_PI);
+
+TEST_CASE("x + (y - x) == y")
+{
+  for(int i = 0; i < 1000; ++i)
+  {
+    auto const x = SO2Element<Scalar_t>(-dis(gen));
+    auto const y = SO2Element<Scalar_t>(-dis(gen));
+    FAST_CHECK_EQ((x + (y - x)).getValue(), doctest::Approx(y.getValue()));
+  }
+}
+
+TEST_CASE("(x + d) - x == d")
+{
+  for(int i = 0; i < 1000; ++i)
+  {
+    auto const x = SO2Element<Scalar_t>(-dis(gen));
+    auto const d = SO2Element<Scalar_t>::TangentVec(-dis(gen));
+
+    FAST_CHECK_EQ(((x + d) - x).value(), doctest::Approx(d.value()));
+  }
+}
+
+TEST_CASE("||(x + d1) - (x + d2)|| <= ||d1 - d2||")
+{
+  for(int i = 0; i < 1000; ++i)
+  {
+    auto const x = SO2Element<Scalar_t>(-dis(gen));
+    auto const d1 = SO2Element<Scalar_t>::TangentVec(-dis(gen));
+    auto const d2 = SO2Element<Scalar_t>::TangentVec(-dis(gen));
+
+    FAST_CHECK_LE(((x + d1) - (x + d2)).norm(), doctest::Approx((d1 - d2).norm()));
+  }
+}
